7330fc8d41fa45b756dd7b982d22993faa205007
Rishab Raj <rr0948555@gmail.com>
2026-08-12 11:41:58 +0530

    Static Assets

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <head>
+           <link rel="stylesheet" href="/style.css" />
+           {/* <script src="/script.js" async/> */}
+      </head>
       <body>{children}</body>
     </html>
   );
---
+++ src/app/static-assets/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const StaticAssets = () => {
+  return (
+    <>
+       <h2>Static Assets In Next JS</h2>
+       <img src="/static.avif" alt="Developer" width={500} height={500}/>
+    </>
+  )
+}
+
+export default StaticAssets
