afe8f355346ffd8f3783faddf3fd87d70c86bf1e
Rishab Raj <rr0948555@gmail.com>
2026-08-12 12:33:40 +0530

    Static Site Generation

--- src/middleware.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-const isLoggedIn = true;
-export function middleware(request) {
-   if(!isLoggedIn && request.nextUrl.pathname != '/home'){
-      return NextResponse.redirect(new URL('/home', request.url))
-   }
-}
