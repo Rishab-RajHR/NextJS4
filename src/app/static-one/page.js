301af9620303301a4cb06cf2b2dbde578c86c903
Rishab Raj <rr0948555@gmail.com>
2026-08-12 12:00:07 +0530

    Static HTML Pages Build

---
+++ src/app/static-one/page.js
@@ -0,0 +1,12 @@
+import Hello from "@/components/Hello"
+
+const StaticOne = () => {
+  return (
+    <>
+        <h2>1) Static HTML Pages</h2>
+        <Hello />
+    </>
+  )
+}
+
+export default StaticOne
---
+++ src/app/static-two/page.js
@@ -0,0 +1,12 @@
+import Hello from "@/components/Hello";
+
+const StaticTwo = () => {
+  return (
+    <>
+        <h2>2) Static HTML Pages</h2>
+        <Hello />
+    </>
+  )
+}
+
+export default StaticTwo;
