619cd07f43bff9b7e35bf59fcbfd63c544ea6dd5
Rishab Raj <rr0948555@gmail.com>
2026-08-13 10:57:28 +0530

    POST API With Static Data

---
+++ src/db/db.js
@@ -0,0 +1,20 @@
+export const employee = [
+    {
+        employeeId : 1,
+        name: 'Alex',
+        salary: '70k',
+        email: 'alex12@gmail.com'
+    },
+    {
+        employeeId : 2,
+        name: 'Basil',
+        salary: '50k',
+        email: 'basil132@gmail.com'
+    },
+    {
+        employeeId : 3,
+        name: 'Tovino',
+        salary: '90k',
+        email: 'tovino35@gmail.com'
+    }
+]
